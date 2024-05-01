70d0f4406f02d54d20d8ed424581c620c7885943
Simon Egli <eglisi1@students.zhaw.ch>
2024-05-01 09:43:36 +0200

    init spring app

---
+++ src/main/java/ch/simi1892/busrental/BusRentalApplication.java
@@ -0,0 +1,13 @@
+package ch.simi1892.busrental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusRentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusRentalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/simi1892/busrental/BusRentalApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.simi1892.busrental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusRentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
